562d0f6da2f3ea74bc49ca569a419e1af571e13f
xiumu <13456817650@163.com>
2016-11-28 08:59:24 +0800

    route测试代码

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import './index.html';
+import './index.css';
+import dva from 'dva';
+import { browserHistory } from 'dva/router'
+// 1. Initialize
+const app = dva({
+  history: browserHistory
+});
+
+// 2. Plugins
+//app.use({});
+
+// 3. Model
+//app.model(require('./models/example'));
+
+// 4. Router
+app.router(require('./router'));
+
+// 5. Start
+app.start('#root');
